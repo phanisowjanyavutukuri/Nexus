d73c50a620f8ba3665963185eaefd6d125aca11e
Nagaraj <rapellinagaraj@gmail.com>
2017-10-31 10:42:16 +0530

    Update App.java

--- src/main/java/com/rns/in/App.java
+++ src/main/java/com/rns/in/App.java
@@ -6,7 +6,7 @@ package com.rns.in;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] arg )
     {
         System.out.println( "Hello World!" );
     }
